b805ff0ca3e28af3b1fb933bfd12a1bda261f1ae
Arif ORAK <ariforak@gmail.com>
2024-02-03 21:14:44 +0300

    Loglama mekanizması kuruldu, bunun için Nlog kütüphanesi projeye dahil edildi.

--- BookStoreApp.Service/Extensions/Microsoft/DependencyResolversForServices.cs
+++ BookStoreApp.Service/Extensions/Microsoft/DependencyResolversForServices.cs
@@ -9,6 +9,7 @@ namespace BookStoreApp.Service.Extensions.Microsoft
         public static void ConfigureServicesForBusinesLayer(this IServiceCollection services)
         {
             services.AddScoped<IServiceManager, ServiceManager>();
+            services.AddSingleton<ILoggerService,LoggerManager>(); // Singletonb sadece bir kez oluşacak olan yapıyı tüm uygulama kullanacak.
         }
     }
 }
--- BookStoreApp.Service/Services/BookManager.cs
+++ BookStoreApp.Service/Services/BookManager.cs
@@ -7,28 +7,29 @@ namespace BookStoreApp.Service.Services
     public class BookManager : IBookService
     {
         private readonly IRepositoryManager _repositoryManager;
+        private readonly ILoggerService _loggerService;
 
-        public BookManager(IRepositoryManager repositoryManager)
+        public BookManager(IRepositoryManager repositoryManager, ILoggerService loggerService)
         {
             _repositoryManager = repositoryManager;
+            _loggerService = loggerService;
         }
 
         public Book CreateOneBook(Book book)
         {
-             if (book == null)
-            {
-                throw new ArgumentNullException(nameof(book));
-            }
-             _repositoryManager.BookRepository.CreateOneBook(book);
+            _repositoryManager.BookRepository.CreateOneBook(book);
             _repositoryManager.Save();
-            return book;    
+            return book;
         }
 
         public void DeleteOneBook(int bookId, bool trackChanges)
         {
-            var deletedEntity = _repositoryManager.BookRepository.GetOneBookById(bookId,trackChanges);
+            var deletedEntity = _repositoryManager.BookRepository.GetOneBookById(bookId, trackChanges);
             if (deletedEntity == null)
+            {
+                _loggerService.LogInfo($"The book with id : {bookId} could not found.");
                 throw new Exception($"Book with id : {bookId} could not found.");
+            }
             _repositoryManager.BookRepository.DeleteOneBook(deletedEntity);
             _repositoryManager.Save();
         }
@@ -42,20 +43,32 @@ namespace BookStoreApp.Service.Services
         {
             var currentEntity = _repositoryManager.BookRepository.GetOneBookById(bookId, trackChanges);
             if (currentEntity == null)
-                throw new Exception($"Book with id : {bookId} could not found.");
+            {
+                string message = $"Book with id : {bookId} could not found.";
+                _loggerService.LogInfo(message);
+                throw new Exception(message);
+            }
 
             return currentEntity;
         }
 
-        public Book UpdateOneBook(int bookId,Book book,bool trackChanges)
+        public Book UpdateOneBook(int bookId, Book book, bool trackChanges)
         {
             var currentEntity = _repositoryManager.BookRepository.GetOneBookById(bookId, trackChanges);
-            if(currentEntity == null)
-                throw new Exception($"Book with id : {bookId} could not found.");
-            if(book is null)
-                throw new ArgumentNullException(nameof(book));
+            if (currentEntity == null)
+            {
+                string logMessage = $"The book with id : {bookId} could not found.";
+                _loggerService.LogInfo(logMessage);
+                throw new Exception(logMessage);
+
+            }
+
             if (bookId != book.Id)
-                throw new Exception($"Parametre olarak gönderilen varlığa ait id bilgisi ile rotadan gönderilen id'ler eşleşmemektedir.");
+            {
+                string message = $"Parametre olarak gönderilen varlığa ait id bilgisi ile rotadan gönderilen id'ler eşleşmemektedir.";
+                _loggerService.LogInfo(message);
+                throw new Exception(message);
+            }
 
             _repositoryManager.BookRepository.UpdateOneBook(book);
             //currentEntity.Title = book.Title;
