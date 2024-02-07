7f4a1253f49ac1db742432b0a3b62dd4e3b1da26
Arif ORAK <ariforak@gmail.com>
2024-02-07 19:39:48 +0300

    Logdetails classı düzenlendi, LogFilters hataları giderildi.

--- BookStoreApp.Core/ResponseResultPattern/LogModel/LogDetails.cs
+++ BookStoreApp.Core/ResponseResultPattern/LogModel/LogDetails.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json;
 
 namespace BookStoreApp.Core.ResponseResultPattern.LogModel
 {
     public class LogDetails
     {
+        public Object? ModelName { get; set; }
+        public Object? ControllerName { get; set; }
+        public Object? ActionName { get; set; }
+        public Object? Id { get; set; }
+        public Object? CreatedAt { get; set; }
+
+        public LogDetails()
+        {
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
     }
 }
